b5d9e4f96efb28a9988575f47dfcabff845c2a60
tr22014 <taharami22014@gmail.com>
2026-07-19 18:55:46 +0100

    commit

--- backend/create_tables.py
+++
@@ -1,6 +0,0 @@
-from database.database import Base, engine
-from app import models  # important : force l'import de tous les modèles
-
-print("Création des tables...")
-Base.metadata.create_all(bind=engine)
-print("✅ Tables créées avec succès !")
